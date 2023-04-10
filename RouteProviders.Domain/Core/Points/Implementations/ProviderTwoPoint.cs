0075bdf4b2837843ca7c65374747be2fe0a4684e
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-10 21:15:08 +0300

    feat: add provider two point implementation

---
+++ RouteProviders.Domain/Core/Points/Implementations/ProviderTwoPoint.cs
@@ -0,0 +1,9 @@
+﻿using RouteProviders.Domain.Core.Points.Abstractions;
+
+namespace RouteProviders.Domain.Core.Points.Implementations;
+
+public class ProviderTwoPoint : Point
+{
+    public ProviderTwoPoint(string point, DateTime date)
+        : base(point, date) { }
+}
