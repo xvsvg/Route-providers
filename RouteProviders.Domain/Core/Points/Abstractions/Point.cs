15d188c95e10d1c434c22bea2e8edf15ddd75d47
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-10 21:14:44 +0300

    feat: add abstract point

---
+++ RouteProviders.Domain/Core/Points/Abstractions/Point.cs
@@ -0,0 +1,15 @@
+﻿using RouteProviders.Domain.Core.Utils;
+
+namespace RouteProviders.Domain.Core.Points.Abstractions;
+
+public abstract class Point
+{
+    protected Point(string name, DateTime date)
+    {
+        Name = Validator.ValidatePoint(name);
+        Date = date;
+    }
+
+    public string Name { get; }
+    public DateTime Date { get; }
+}
