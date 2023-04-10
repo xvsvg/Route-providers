611507e52bdcfd5a8b486de132ee42297e4683f6
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-11 00:23:52 +0300

    feat: add persistence DI extensions

---
+++ RouteProviders.Persistence/DataAccess/Extensions/RegistrationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RouteProviders.Application.Contracts.Persistence;
+using RouteProviders.Persistence.DataAccess.DatabaseContext;
+
+namespace RouteProviders.Persistence.DataAccess.Extensions;
+
+public static class RegistrationExtensions
+{
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection collection,
+        Action<DbContextOptionsBuilder> action)
+    {
+        collection.AddDbContext<IRouteProvidersDatabaseContext, RouteProvidersDatabaseContext>(action);
+
+        return collection;
+    }
+}
