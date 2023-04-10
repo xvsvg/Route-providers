22721ac95afe664d8e6de74941c6b7b5f7c7ca42
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-11 00:22:46 +0300

    feat: add application db context

---
+++ RouteProviders.Persistence/DataAccess/DatabaseContext/RouteProvidersDatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RouteProviders.Application.Contracts.Persistence;
+using RouteProviders.Domain.Core.Routes.Abstractions;
+
+namespace RouteProviders.Persistence.DataAccess.DatabaseContext;
+
+public class RouteProvidersDatabaseContext : DbContext, IRouteProvidersDatabaseContext
+{
+    public RouteProvidersDatabaseContext(DbContextOptions<RouteProvidersDatabaseContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Route> Routes { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IAssemblyMarker).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
