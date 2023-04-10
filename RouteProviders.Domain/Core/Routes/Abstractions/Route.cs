6bc5b6a96486cc714d7b10eca3db33922dd9edb5
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-10 21:15:29 +0300

    feat: add abstract route

---
+++ RouteProviders.Domain/Core/Routes/Abstractions/Route.cs
@@ -0,0 +1,25 @@
+﻿using RouteProviders.Domain.Core.Utils;
+
+namespace RouteProviders.Domain.Core.Routes.Abstractions;
+
+public abstract class Route
+{
+    private decimal _price;
+
+    protected Route(decimal price, DateTime timeLimit)
+    {
+        Price = price;
+        TimeLimit = Validator.ValidateTimeLimit(timeLimit);
+
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; }
+    public decimal Price
+    {
+        get => _price;
+        set => _price = Validator.ValidatePrice(value);
+    }
+
+    public DateTime TimeLimit { get; }
+}
