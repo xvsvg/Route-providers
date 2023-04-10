03be928d3d1f64c5055f5820e399d50bd7b17a6b
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-11 00:20:19 +0300

    feat: add database context contract

---
+++ RouteProviders.Application/Contracts/Persistence/IRouteProvidersDatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RouteProviders.Domain.Core.Routes.Abstractions;
+
+namespace RouteProviders.Application.Contracts.Persistence;
+
+public interface IRouteProvidersDatabaseContext
+{
+    DbSet<Route> Routes { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
