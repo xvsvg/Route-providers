e83e762d331b6527e67d0ad995890f8077790d57
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-12 00:20:10 +0300

    feat: modify entrypoint

--- RouteProviders.Presentation/Program.cs
+++ RouteProviders.Presentation/Program.cs
@@ -1,5 +1,30 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using RouteProviders.Application.Extensions;
+using RouteProviders.Persistence.DataAccess.Extensions;
+using RouteProviders.Persistence.DataAccess.Helpers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddPersistence(o => o.UseLazyLoadingProxies().UseSqlite("Data Source=RP.db"));
+builder.Services.AddApplication();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddResponseCaching();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI();
+    app.UseSwagger();
+
+    using (IServiceScope scope = app.Services.CreateScope())
+    {
+        await SeedingHelper.SeedData(scope);
+    }
+}
+
+app.UseResponseCaching();
+app.MapControllers();
 app.Run();
