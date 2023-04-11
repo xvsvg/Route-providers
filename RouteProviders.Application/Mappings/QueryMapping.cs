0407bb7d804a4f48348f4dcae1ebe06ea8d52c41
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-12 00:17:34 +0300

    feat: add mappings

---
+++ RouteProviders.Application/Mappings/QueryMapping.cs
@@ -0,0 +1,27 @@
+﻿using RouteProviders.Application.Contracts.Search.Queries;
+
+namespace RouteProviders.Application.Mappings;
+
+public static class QueryMapping
+{
+    public static ProviderTwoSearch.Query AsProviderTwoQuery(this Search.Query query)
+    {
+        return new ProviderTwoSearch.Query(
+            Departure: query.Origin,
+            Arrival: query.Destination,
+            DepartureDate: query.OriginDateTime,
+            MinTimeLimit: query.Filter?.MinTimeLimit
+            );
+    }
+
+    public static ProviderOneSearch.Query AsProviderOneQuery(this Search.Query query)
+    {
+        return new ProviderOneSearch.Query(
+            From: query.Origin,
+            To: query.Destination,
+            DateFrom: query.OriginDateTime,
+            DateTo: query.Filter?.DestinationDateTime,
+            MaxPrice: query.Filter?.MaxPrice
+            );
+    }
+}
--- RouteProviders.Application/Mappings/RouteMapping.cs
+++ RouteProviders.Application/Mappings/RouteMapping.cs
@@ -1,4 +1,5 @@
 ﻿using RouteProviders.Application.Dto;
+using RouteProviders.Domain.Core.Points.Implementations;
 using RouteProviders.Domain.Core.Routes.Abstractions;
 using RouteProviders.Domain.Core.Routes.Implementations;
 
@@ -6,6 +7,42 @@ namespace RouteProviders.Application.Mappings;
 
 public static class RouteMapping
 {
+    public static IEnumerable<RouteDto> AsRouteDtos(this List<ProviderOneRoute>? routes)
+    {
+        var result = new List<RouteDto>();
+
+        routes?.ForEach(r => result.Add(r.AsDto()));
+
+        return result;
+    }
+
+    public static IEnumerable<RouteDto> AsRouteDtos(this List<ProviderTwoRoute>? routes)
+    {
+        var result = new List<RouteDto>();
+
+        routes?.ForEach(r => result.Add(r.AsDto()));
+
+        return result;
+    }
+
+    public static IEnumerable<ProviderOneRouteDto> AsProviderRouteDtos(this List<ProviderOneRoute>? routes)
+    {
+        var result = new List<ProviderOneRouteDto>();
+
+        routes?.ForEach(r => result.Add(r.AsProviderOneRouteDto()));
+
+        return result;
+    }
+
+    public static IEnumerable<ProviderTwoRouteDto> AsProviderRouteDtos(this List<ProviderTwoRoute>? routes)
+    {
+        var result = new List<ProviderTwoRouteDto>();
+
+        routes?.ForEach(r => result.Add(r.AsProviderTwoRouteDto()));
+
+        return result;
+    }
+
     public static RouteDto AsDto(this ProviderOneRoute route)
     {
         return new RouteDto(
@@ -32,21 +69,33 @@ public static class RouteMapping
         );
     }
 
-    public static IEnumerable<RouteDto> AsDto(this List<ProviderOneRoute>? routes)
+    public static ProviderOneRouteDto AsProviderOneRouteDto(this ProviderOneRoute route)
     {
-        var result = new List<RouteDto>();
-
-        routes?.ForEach(r => result.Add(r.AsDto()));
-
-        return result;
+        return new ProviderOneRouteDto(
+            From: route.From,
+            To: route.To,
+            DateFrom: route.DateFrom,
+            DateTo: route.DateTo,
+            Price: route.Price,
+            TimeLimit: route.TimeLimit
+            );
     }
 
-    public static IEnumerable<RouteDto> AsDto(this List<ProviderTwoRoute>? routes)
+    public static ProviderTwoRouteDto AsProviderTwoRouteDto(this ProviderTwoRoute route)
     {
-        var result = new List<RouteDto>();
+        return new ProviderTwoRouteDto(
+            Departure: route.Departure.AsDto(),
+            Arrival: route.Arrival.AsDto(),
+            Price: route.Price,
+            TimeLimit: route.TimeLimit
+        );
+    }
 
-        routes?.ForEach(r => result.Add(r.AsDto()));
-
-        return result;
+    public static ProviderTwoPointDto AsDto(this ProviderTwoPoint point)
+    {
+        return new ProviderTwoPointDto(
+            Point: point.Name,
+            Date: point.Date
+            );
     }
 }
