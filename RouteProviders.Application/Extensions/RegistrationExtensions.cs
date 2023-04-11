7d0e473398306ca51dc42c05e9858687b75d7b4f
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-12 00:16:10 +0300

    feat: add application DI

---
+++ RouteProviders.Application/Extensions/RegistrationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RouteProviders.Application.Extensions;
+
+public static class RegistrationExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection collection)
+    {
+        collection.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(typeof(IAssemblyMarker).Assembly));
+
+        return collection;
+    }
+}
