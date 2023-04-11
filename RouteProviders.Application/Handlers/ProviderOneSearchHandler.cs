b1e5853ff8535f7355eeab7d7a6deb6e07b17b2a
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-12 00:16:24 +0300

    feat: add handlers

---
+++ RouteProviders.Application/Handlers/ProviderOneSearchHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RouteProviders.Application.Contracts.Persistence;
+using RouteProviders.Application.Contracts.Search.Queries;
+using RouteProviders.Application.Mappings;
+using RouteProviders.Domain.Core.Routes.Implementations;
+
+namespace RouteProviders.Application.Handlers;
+
+internal class ProviderOneSearchHandler : IRequestHandler<ProviderOneSearch.Query, ProviderOneSearch.Response>
+{
+    private readonly IRouteProvidersDatabaseContext _context;
+
+    public ProviderOneSearchHandler(IRouteProvidersDatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ProviderOneSearch.Response> Handle(ProviderOneSearch.Query request, CancellationToken cancellationToken)
+    {
+        var response = await _context.Routes.OfType<ProviderOneRoute>()
+            .Where(r =>
+                r.DateTo == (request.DateTo ?? r.DateTo) &&
+                r.DateFrom == request.DateFrom &&
+                r.From == request.From &&
+                r.To == request.To &&
+                r.Price <= request.MaxPrice)
+            .ToListAsync(cancellationToken);
+
+        return new ProviderOneSearch.Response(response.AsProviderRouteDtos());
+    }
+}
---
+++ RouteProviders.Application/Handlers/ProviderTwoSearchHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RouteProviders.Application.Contracts.Persistence;
+using RouteProviders.Application.Contracts.Search.Queries;
+using RouteProviders.Application.Mappings;
+using RouteProviders.Domain.Core.Routes.Implementations;
+
+namespace RouteProviders.Application.Handlers;
+
+internal class ProviderTwoSearchHandler : IRequestHandler<ProviderTwoSearch.Query, ProviderTwoSearch.Response>
+{
+    private readonly IRouteProvidersDatabaseContext _context;
+
+    public ProviderTwoSearchHandler(IRouteProvidersDatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ProviderTwoSearch.Response> Handle(ProviderTwoSearch.Query request, CancellationToken cancellationToken)
+    {
+        var response = await _context.Routes.OfType<ProviderTwoRoute>()
+            .Where(r =>
+                r.Arrival.Name == request.Arrival &&
+                r.Departure.Name == request.Departure &&
+                r.Arrival.Date == request.DepartureDate &&
+                r.TimeLimit >= (request.MinTimeLimit ?? DateTime.Now))
+            .ToListAsync(cancellationToken);
+
+        return new ProviderTwoSearch.Response(response.AsProviderRouteDtos());
+    }
+}
