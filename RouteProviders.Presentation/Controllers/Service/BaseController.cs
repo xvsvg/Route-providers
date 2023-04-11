1db7b6bd371fe1441b92e5e15b01aa8a083bae16
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-12 00:19:27 +0300

    feat: add controllers using services

---
+++ RouteProviders.Presentation/Controllers/Service/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RouteProviders.Application.Contracts.Persistence;
+using RouteProviders.Application.Contracts.Search.Services;
+#pragma warning disable CS8618
+
+namespace RouteProviders.Presentation.Controllers.Service;
+
+[ApiController]
+[Route("[controller]/api/v1")]
+public abstract class BaseController : ControllerBase
+{
+    protected ISearchService _service;
+}
---
+++ RouteProviders.Presentation/Controllers/Service/ProviderTwoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RouteProviders.Application.Contracts.Persistence;
+using RouteProviders.Application.Contracts.Search.Queries;
+using RouteProviders.Application.Implementations.Services;
+
+namespace RouteProviders.Presentation.Controllers.Service;
+
+[ResponseCache(Duration = 60,
+    Location = ResponseCacheLocation.Any,
+    VaryByQueryKeys = new string[] { "latest" })]
+public class ProviderTwoController : BaseController
+{
+    public ProviderTwoController(IRouteProvidersDatabaseContext context)
+    {
+        _service = new ProviderTwoService(context);
+    }
+
+    [HttpGet("ping")]
+    public async Task<IActionResult> IsAvailable(CancellationToken cancellationToken)
+    {
+        var response = await _service.IsAvailableAsync(cancellationToken);
+
+        return response ? StatusCode(StatusCodes.Status200OK) : StatusCode(StatusCodes.Status500InternalServerError);
+    }
+
+    [HttpPost("search")]
+    public async Task<ActionResult<Search.Response>> FindRoutes(
+        Search.Query query,
+        CancellationToken cancellationToken)
+    {
+        return StatusCode(StatusCodes.Status200OK, await _service.SearchAsync(query, cancellationToken));
+    }
+}
