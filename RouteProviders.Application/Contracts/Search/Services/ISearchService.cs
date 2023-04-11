980df974dc421adbffae3376023951c181f5e02a
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-11 21:07:09 +0300

    feat: add search service contract

---
+++ RouteProviders.Application/Contracts/Search/Services/ISearchService.cs
@@ -0,0 +1,7 @@
+﻿namespace RouteProviders.Application.Contracts.Search.Services;
+
+public interface ISearchService
+{
+    Task<Queries.Search.Response> SearchAsync(Queries.Search.Query request, CancellationToken cancellationToken);
+    Task<bool> IsAvailableAsync(CancellationToken cancellationToken);
+}
