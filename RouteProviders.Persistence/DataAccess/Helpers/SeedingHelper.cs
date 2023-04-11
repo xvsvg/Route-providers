a7eaea6fe535067ec60af195ffab88d6cf63c6af
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-12 00:18:22 +0300

    feat: add data seeder

---
+++ RouteProviders.Persistence/DataAccess/Helpers/SeedingHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RouteProviders.Domain.Core.Routes.Implementations;
+using RouteProviders.Persistence.DataAccess.DatabaseContext;
+
+namespace RouteProviders.Persistence.DataAccess.Helpers;
+
+public static class SeedingHelper
+{
+    public static Task SeedData(IServiceScope scope)
+    {
+        SeedData(scope.ServiceProvider.GetService<RouteProvidersDatabaseContext>());
+
+        return Task.CompletedTask;
+    }
+
+    private static void SeedData(RouteProvidersDatabaseContext? context)
+    {
+        if (context != null && !context.Routes.Any())
+        {
+            context.Routes.AddRange(
+                new ProviderOneRoute("Moscow", "Sochi", DateTime.Today, DateTime.Today.AddDays(2), 200, DateTime.MaxValue),
+                new ProviderOneRoute("Sochi", "Moscow", DateTime.Today.AddDays(2), DateTime.Today.AddDays(20), 300, DateTime.MaxValue),
+                new ProviderOneRoute("Bankog", "Taganrog", DateTime.Today.AddDays(5), DateTime.Today.AddDays(30), 400, DateTime.MaxValue)
+            );
+        }
+        else Console.WriteLine("Db wasn't created");
+    }
+}
