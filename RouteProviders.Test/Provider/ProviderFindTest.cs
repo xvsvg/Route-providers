17ab58dd28e059ef848de6dc4dc9b523098ba850
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-12 14:20:11 +0300

    feat: add db search test

---
+++ RouteProviders.Test/Provider/ProviderFindTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RouteProviders.Domain.Core.Routes.Implementations;
+
+namespace RouteProviders.Test.Provider;
+
+public class ProviderFindTest : TestBase
+{
+    [Fact]
+    public async Task FindInDatabaseTest_ProviderOne()
+    {
+        var response = await Context.Routes.OfType<ProviderOneRoute>()
+            .ToListAsync();
+
+        Assert.Equal(2, response.Count);
+    }
+
+    [Fact]
+    public async Task FindInDatabaseTest_ProviderTwo()
+    {
+        var response = await Context.Routes.OfType<ProviderTwoRoute>()
+            .ToListAsync();
+
+        Assert.Single(response);
+    }
+}
