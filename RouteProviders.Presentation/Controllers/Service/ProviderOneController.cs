166a25335797b3ab92cd77550d705509f67d91f9
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-12 13:29:08 +0300

    feat: get rid of ResponseCache attributed in service-controller, now using DistributedMemoryCache

--- RouteProviders.Presentation/Controllers/Service/ProviderOneController.cs
+++ RouteProviders.Presentation/Controllers/Service/ProviderOneController.cs
@@ -1,18 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using RouteProviders.Application.Contracts.Persistence;
 using RouteProviders.Application.Contracts.Search.Queries;
 using RouteProviders.Application.Implementations.Services;
+using RouteProviders.Presentation.Contracts.Search.Filters;
 
 namespace RouteProviders.Presentation.Controllers.Service;
 
-[ResponseCache(Duration = 60,
-    Location = ResponseCacheLocation.Any,
-    VaryByQueryKeys = new string[] { "latest" })]
 public class ProviderOneController : BaseController
 {
-    public ProviderOneController(IRouteProvidersDatabaseContext context)
+    private readonly IDistributedCache _cache;
+
+    public ProviderOneController(IRouteProvidersDatabaseContext context, IDistributedCache cache)
     {
         _service = new ProviderOneService(context);
+        _cache = cache;
     }
 
     [HttpGet("ping")]
@@ -25,9 +28,48 @@ public class ProviderOneController : BaseController
 
     [HttpPost("search")]
     public async Task<ActionResult<Search.Response>> FindRoutes(
-        Search.Query query,
+        [FromBody] Search.Query query,
         CancellationToken cancellationToken)
     {
-        return StatusCode(StatusCodes.Status200OK, await _service.SearchAsync(query, cancellationToken));
+        if (query.Filter?.OnlyCached is true)
+        {
+            var response = await _cache.GetStringAsync(query.ToString(), cancellationToken);
+
+            var result = await Task.Factory.StartNew(() =>
+                JsonConvert.DeserializeObject<Search.Response>(response ?? string.Empty), cancellationToken);
+
+            return StatusCode(StatusCodes.Status200OK, result);
+        }
+
+        try
+        {
+            var dbResponse = await _service.SearchAsync(query, cancellationToken);
+
+            var serializedResponse = await Task.Factory.StartNew(() =>
+                JsonConvert.SerializeObject(dbResponse), cancellationToken);
+
+            var key = new Search.Query(
+                query.Origin,
+                query.Destination,
+                query.OriginDateTime,
+                new Filter(
+                    query.Filter?.DestinationDateTime,
+                    query.Filter?.MaxPrice,
+                    query.Filter?.DestinationDateTime,
+                    true
+                ));
+
+            await _cache.SetStringAsync(key.ToString(), serializedResponse, new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5)
+            }, cancellationToken);
+
+
+            return StatusCode(StatusCodes.Status200OK, dbResponse);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status404NotFound, ex.Message);
+        }
     }
 }
--- RouteProviders.Presentation/Controllers/Service/ProviderTwoController.cs
+++ RouteProviders.Presentation/Controllers/Service/ProviderTwoController.cs
@@ -1,18 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using RouteProviders.Application.Contracts.Persistence;
 using RouteProviders.Application.Contracts.Search.Queries;
 using RouteProviders.Application.Implementations.Services;
+using RouteProviders.Presentation.Contracts.Search.Filters;
 
 namespace RouteProviders.Presentation.Controllers.Service;
 
-[ResponseCache(Duration = 60,
-    Location = ResponseCacheLocation.Any,
-    VaryByQueryKeys = new string[] { "latest" })]
 public class ProviderTwoController : BaseController
 {
-    public ProviderTwoController(IRouteProvidersDatabaseContext context)
+    private readonly IDistributedCache _cache;
+
+    public ProviderTwoController(IRouteProvidersDatabaseContext context, IDistributedCache cache)
     {
         _service = new ProviderTwoService(context);
+        _cache = cache;
     }
 
     [HttpGet("ping")]
@@ -28,6 +31,38 @@ public class ProviderTwoController : BaseController
         Search.Query query,
         CancellationToken cancellationToken)
     {
+        if (query.Filter?.OnlyCached is true)
+        {
+            var response = await _cache.GetStringAsync(query.ToString(), cancellationToken);
+
+            var result = await Task.Factory.StartNew(() =>
+                JsonConvert.DeserializeObject<Search.Response>(response ?? string.Empty), cancellationToken);
+
+            return StatusCode(StatusCodes.Status200OK, result);
+        }
+
+        var dbResponse = _service.SearchAsync(query, cancellationToken);
+
+        var key = new Search.Query(
+            query.Origin,
+            query.Destination,
+            query.OriginDateTime,
+            new Filter(
+                query.Filter?.DestinationDateTime,
+                query.Filter?.MaxPrice,
+                query.Filter?.DestinationDateTime,
+                true
+            ));
+
+        var serializedResponse = await Task.Factory.StartNew(() =>
+            JsonConvert.SerializeObject(dbResponse), cancellationToken);
+
+        await _cache.SetStringAsync(key.ToString(), serializedResponse, new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10)
+        }, cancellationToken);
+
+
         return StatusCode(StatusCodes.Status200OK, await _service.SearchAsync(query, cancellationToken));
     }
 }
