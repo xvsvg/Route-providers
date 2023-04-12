e1b0e2241bb8e9c0b557ee84990c719edab5c795
疯 狂的冲刺 <mdcrtboy@mail.ru>
2023-04-12 14:19:42 +0300

    feat: add test base class

---
+++ RouteProviders.Test/TestBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RouteProviders.Persistence.DataAccess.DatabaseContext;
+using RouteProviders.Persistence.DataAccess.Extensions;
+using RouteProviders.Persistence.DataAccess.Helpers;
+
+namespace RouteProviders.Test;
+
+public class TestBase : IDisposable
+{
+    public TestBase()
+    {
+        var collection = new ServiceCollection();
+        var id = Guid.NewGuid();
+
+        collection.AddPersistence(o =>
+            o.UseSqlite($"Data Source={id}.db").UseLazyLoadingProxies());
+
+        Provider = collection.BuildServiceProvider();
+        Context = Provider.GetRequiredService<RouteProvidersDatabaseContext>();
+        Context.Database.EnsureCreated();
+
+        using var scope = Provider.CreateScope();
+        SeedingHelper.SeedData(scope);
+    }
+
+    protected RouteProvidersDatabaseContext Context { get; }
+    protected IServiceProvider Provider { get; }
+
+    public void Dispose()
+    {
+        Context.Database.EnsureDeleted();
+        Context.Dispose();
+    }
+}
